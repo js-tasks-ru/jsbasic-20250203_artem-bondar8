1b0c3fd1a92c14922427b6ac4eb5383dbfc62e9d
Artem Bondar <js.learn.13@gmail.com>
2025-02-26 10:51:46 -0500

    Add carousel initialization with navigation arrows

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
-}
+  const carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  const carouselInner = document.querySelector('.carousel__inner');
+  const carouselSlides = document.querySelectorAll('.carousel__slide');
+  const totalSlides = carouselSlides.length;
+  let currentSlide = 0;
+
+  carouselArrowLeft.style.display = 'none';
+
+  function updateCarousel() {
+    carouselInner.style.transform = `translateX(${-currentSlide * 100}%)`;
+
+    carouselArrowLeft.style.display = currentSlide === 0 ? 'none' : '';
+    carouselArrowRight.style.display = currentSlide === totalSlides - 1 ? 'none' : '';
+  };
+
+  function rightArrow() {
+    if (currentSlide < totalSlides - 1) {
+      currentSlide++;
+      updateCarousel();
+    };
+  };
+
+  function leftArrow() {
+    if (currentSlide > 0) {
+      currentSlide--;
+      updateCarousel();
+    };
+  };
+
+  carouselArrowRight.addEventListener('click', rightArrow);
+  carouselArrowLeft.addEventListener('click', leftArrow);
+};
